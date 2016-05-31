9d0f8a54eb8b037e1f4e0d1d09ec24d96f151494
cloes <375600284@qq.com>
2016-05-31 16:09:29 +0800

    add send email file

---
+++ sendMail.php
@@ -0,0 +1,41 @@
+<?php
+$to  = 'wayne511200295@163.com' . ', '; // note the comma
+//$to  = 'wayne511200295@163.com' . ', '; // note the comma
+//$to .= 'wez@example.com';
+
+// subject
+$subject = 'Login Reminder';
+
+// message
+$message = '
+<html>
+<head>
+  <title>Birthday Reminders for August</title>
+</head>
+<body>
+  <p>Here are the birthdays upcoming in August!</p>
+  <table>
+    <tr>
+      <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
+    </tr>
+    <tr>
+      <td>Joe</td><td>3rd</td><td>August</td><td>1970</td>
+    </tr>
+    <tr>
+      <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
+    </tr>
+  </table>
+</body>
+</html>
+';
+
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Additional headers
+$headers .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
+
+// Mail it
+mail($to, $subject, $message, $headers);
+?>
